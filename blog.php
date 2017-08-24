505f8f96e20f32c331c4cc6d10076412b6f0be79
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2017-08-25 00:54:35 +0800

    更新了blog.php

--- blog.php
+++ blog.php
@@ -112,21 +112,6 @@ Develop by ZhuBrocadeSoar
 					<p><a href="' . $row['topic_index']/*传递文章id*/ . '">' . $row['topic_abstract']/*显示摘要*/ . '</a></p>
 				</li>';
         }
-				<li>
-					<p class="date"><a href="#">Jan<b>03</b></a></p>
-					<h3>Sagittis diam dolor amet</h3>
-					<p><a href="#">Etiam non felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem. Mauris quam enim, molestie. Donec leo, vivamus fermentum nibh in augue praesent congue rutrum.</a></p>
-				</li>
-				<li>
-					<p class="date"><a href="#">Jan<b>01</b></a></p>
-					<h3>Amet sed volutpat mauris</h3>
-					<p><a href="#">Consectetuer adipiscing elit. Nam pede erat, porta eu, lobortis eget, tempus et, tellus. Etiam neque. Vivamus consequat lorem at nisl. Nullam non wisi a sem semper eleifend. Etiam non felis. Donec ut ante.</a></p>
-				</li>
-				<li>
-					<p class="date"><a href="#">Dec<b>31</b></a></p>
-					<h3>Sagittis diam dolor amet</h3>
-					<p><a href="#">Etiam non felis. Donec ut ante. In id eros. Suspendisse lacus turpis, cursus egestas at sem. Mauris quam enim, molestie. Donec leo, vivamus fermentum nibh in augue praesent congue rutrum.</a></p>
-				</li>
         echo '
 			</ul>
 		</div>';
