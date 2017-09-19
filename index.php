9b2e99b1ff469155b8a406f8984b82f32953f89a
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2017-09-19 14:24:13 +0800

    新的index.php

--- index.php
+++ index.php
@@ -36,9 +36,46 @@ https://brocadesoar.cn
 				<div id="div_logo" class="logo">	
 					<a href="https://brocadesoar.cn"><img src="images/logo.png" alt="" height="128" width="128" /></a>	
 					<h1><a href="https://brocadesoar.cn">soar</a></h1>	
-					<span></span>	
+					<span>Develop by <a href="https://brocadesoar.cn" rel="nofollow">ZhuBrocadeSoar</a></span>
+                    <span><p>腹中没有半瓶醋，三分热度写我情。</p><p>文章从来教我改，不敢写一句违心。</p></span>
 				</div>	
+                <div id="div_menu" class="menu">
+                    <ul>
+                        <li><a href="https://brocadesoar.cn" accesskey="1" title ="">主页</a></li>
+                        <li><a href="blog.php" accesskey="2" title="">博客</a></li>
+                        <li><a href="bbs.php" accesskey="3" title="">留言</a></li>
+                        <li><a href="#" accesskey="4" title="">梯子</a></li>
+                        <li><a href="#" accesskey="5" title="">传送</a></li>
+                        <li><a href="#" accesskey="6" title="">关于</a></li>
+                        <li><a href="login.php" accesskey="7" title="">登陆</a></li>
+                    </ul>
+                </div>
 			</div>	
+            <div id="div_main" class="main">
+                <div id="div_banner" class="banner">
+                    <img src="images/aPicWithoutMe.jpg" alt="" class="image-full" height="335" width="250">
+                </div>
+                <div id="div_welcome" class="welcome">
+                    <div id="div_welcome_title" class="title">
+                        <h2>乐躬耕于陇亩兮，吾爱吾庐</h2>
+                        <span class="byline">照片沉淀回忆，或喜或忧，你找不到我，时光的那一头，角落里我的一角背影</span>
+                    </div>
+                    <p>时间会匆匆流逝，其间发生的，美好的、不好的事物总归会在脑海中渐渐遗忘，尤其是像我一样，获得了遗忘的天赋。若是旅行，可以拿相机记录一些时刻，但大多数情况我没能把握那稍纵即逝的刹那。写，是一种记录方式，是一种态度，是一种生活情趣，是一种营销手段。我感谢自己有写的欲望和写的勇气，激励自己要不断前进。</p>
+                </div>
+            </div>
+            <div id="div_copyright" class="copyright">
+                <span>
+                    <a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" rel="license"><img alt="知识共享许可协议" style="border-width:0"i src="images/88x31.png" /></a>
+                    <br />
+                    本作品采用<a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" rel="license">知识共享署名-非商业性使用-禁止演绎 4.0 国际许可协议</a>进行许可。
+                </span>
+                <span>
+                    Template Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>|Site Build by <a href="https://brocadesoar.cn" rel="nofollow">ZhuBrocadeSoar</a>
+                </span>
+                <span><div style="width:300px;margin:0 auto; padding:20px 0;">
+                浙ICP备17027825号-1|<a target=" blank " href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33078302100258" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="images/beianlogo.png" style="float:left;" /><p style="float:left;height:20px;line-height:20px;matgin:0px 0px 0px 5px; color:#939393;">浙公网安备 33078302100258号</p></a>
+                </div></span>
+            </div>
 		</div>	
 	<!-- BODY-END -->	
 	</body>	
