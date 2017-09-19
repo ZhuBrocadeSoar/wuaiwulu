245e0fc6dd3ca8c99828d1b329d32fab3f3f8beb
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2017-09-19 14:33:36 +0800

    bug

--- index.php
+++ index.php
@@ -31,15 +31,15 @@ https://brocadesoar.cn
 	</head>	
 	<!-- BODY-BEGIN -->	
 	<body>	
-		<div id="div_page" class="container">	
-			<div id="div_header" class="header">	
-				<div id="div_logo" class="logo">	
+		<div id="page" class="container">	
+			<div id="header">	
+				<div id="logo">	
 					<a href="https://brocadesoar.cn"><img src="images/logo.png" alt="" height="128" width="128" /></a>	
 					<h1><a href="https://brocadesoar.cn">soar</a></h1>	
 					<span>Develop by <a href="https://brocadesoar.cn" rel="nofollow">ZhuBrocadeSoar</a></span>
                     <span><p>腹中没有半瓶醋，三分热度写我情。</p><p>文章从来教我改，不敢写一句违心。</p></span>
 				</div>	
-                <div id="div_menu" class="menu">
+                <div id="menu">
                     <ul>
                         <li><a href="https://brocadesoar.cn" accesskey="1" title ="">主页</a></li>
                         <li><a href="blog.php" accesskey="2" title="">博客</a></li>
@@ -51,11 +51,11 @@ https://brocadesoar.cn
                     </ul>
                 </div>
 			</div>	
-            <div id="div_main" class="main">
-                <div id="div_banner" class="banner">
+            <div id="main">
+                <div id="banner">
                     <img src="images/aPicWithoutMe.jpg" alt="" class="image-full" height="335" width="250">
                 </div>
-                <div id="div_welcome" class="welcome">
+                <div id="welcome">
                     <div id="div_welcome_title" class="title">
                         <h2>乐躬耕于陇亩兮，吾爱吾庐</h2>
                         <span class="byline">照片沉淀回忆，或喜或忧，你找不到我，时光的那一头，角落里我的一角背影</span>
@@ -63,7 +63,7 @@ https://brocadesoar.cn
                     <p>时间会匆匆流逝，其间发生的，美好的、不好的事物总归会在脑海中渐渐遗忘，尤其是像我一样，获得了遗忘的天赋。若是旅行，可以拿相机记录一些时刻，但大多数情况我没能把握那稍纵即逝的刹那。写，是一种记录方式，是一种态度，是一种生活情趣，是一种营销手段。我感谢自己有写的欲望和写的勇气，激励自己要不断前进。</p>
                 </div>
             </div>
-            <div id="div_copyright" class="copyright">
+            <div id="copyright">
                 <span>
                     <a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" rel="license"><img alt="知识共享许可协议" style="border-width:0"i src="images/88x31.png" /></a>
                     <br />
