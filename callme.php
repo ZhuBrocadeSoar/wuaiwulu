aa4af3497301583be46edeeb8b171226da9792d9
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2017-10-23 21:07:13 +0800

    基本交流

--- callme.php
+++ callme.php
@@ -1,4 +1,4 @@
 <?php
-    $data = array('name' => 'callme', 'content' => 'hello wold');
+    $data = array('name' => 'callme', 'content' => 'hello wold', 'money' => '100', 'x' => $_GET['x'], 'y' => $_GET['y']);
     echo json_encode($data);
 ?>
