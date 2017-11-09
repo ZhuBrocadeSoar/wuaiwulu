413b277f905820738d1fe6926f34b668e513dfab
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2017-11-10 02:56:18 +0800

    callme

--- callme.php
+++ callme.php
@@ -3,10 +3,20 @@
     if(mysqli_connect_errno()){
         echo "Error: " . mysqli_connect_error();
     }
-    $retval = mysqli_query($connToMysql, "SELECT COUNT(*) FROM seller_list");
-    if(!$retval){
-        echo "Error: " . mysqli_error();
+    if(/*isset($_GET['query']*/1)){
+        if(/*$_GET['query'] == "seller_list"*/1){
+            $retval = mysqli_query($connToMysql, "SELECT COUNT(*) FROM seller_list");
+            if(!$retval){
+                echo "Error: " . mysqli_error();
+            }
+            $row = mysqli_fetch_array($retval, MYSQLI_ASSOC);
+            $countList['count'] = $row;
+            $retval = mysqli_query($connToMysql, "SELECT name_seller FROM seller_list");
+            if(!$retval){
+                echo "Error: " . mysqli_error();
+            }
+            $row = mysqli_fetch_array($retval, MYSQLI_ASSOC);
+            echo json_encode(array_merge($countList, $row));
+        }
     }
-    $row = mysqli_fetch_array($retval, MYSQLI_ASSOC);
-    echo json_encode($row);
 ?>
