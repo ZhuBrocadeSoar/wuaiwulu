510ab2a2104e48c3e60894fcc6472f584137c03f
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2017-11-10 02:43:34 +0800

    callme

--- callme.php
+++ callme.php
@@ -3,4 +3,10 @@
     if(mysqli_connect_errno()){
         echo "Error: " . mysqli_connect_error();
     }
+    $retval = mysqli_query($connToMysql, "SELECT COUNT(*) FROM seller_list");
+    if(!$retval){
+        echo "Error: " . mysqli_error();
+    }
+    $row = mysqli_fetch_array($retval, MYSQLI_ASSOC);
+    echo json_encode($row);
 ?>
