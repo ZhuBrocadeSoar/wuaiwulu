4289efb314645452214a1d7adea136122c1690d0
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2017-11-10 03:35:55 +0800

    callme

--- callme.php
+++ callme.php
@@ -1,4 +1,5 @@
 <?php
+$_GET['query'] = "seller_list";
     $connToMysql = mysqli_connect("localhost", "nitmaker_cn", "nitmaker.cn", "callme");
     if(mysqli_connect_errno()){
         echo "Error: " . mysqli_connect_error();
