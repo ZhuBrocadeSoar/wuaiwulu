63f2521a839f6646f6ac4cb69184c998e10adf91
ZhuBrocadeSoar <zhujinteng2012@hotmail.com>
2017-11-10 23:05:38 +0800

    开发吾爱吾庐个人博客

--- callme.php
+++ callme.php
@@ -1,11 +1,48 @@
 <?php
-//$_GET['query'] = "seller_list";
+    //$_GET['query'] = "seller_list"; // 测试
+    // 连接数据库
     $connToMysql = mysqli_connect("localhost", "nitmaker_cn", "nitmaker.cn", "callme");
     if(mysqli_connect_errno()){
         echo "Error: " . mysqli_connect_error();
     }
-    if(isset($_GET['query'])){
-        if($_GET['query'] == "seller_list"){
+    // 处理请求
+    if(isset($_GET['query'] && isset($_GET['sessionkey']))){
+        // 检查session
+        $retval = mysqli_query($connToMysql, "SELECT openid, sessionkey, time_session FROM session_record WHERE sessionkey = " . $_GET['sessionkey']);
+        $row = mysqli_fetch_array($retval, MYSQLI_NUM);
+        if($row == NULL){
+            echo "Error: need login";
+        }else{
+            $now = date("Y-m-d H:i:s", time());
+            $datatimediff = date_diff($now, $row[2]);
+            if($datatimediff['y'] > 0 || $datatimediff['m'] > 0 || $datatimediff['d'] > 0 || $datatimediff['h'] > 6){
+                echo "Error: time out";
+            }
+        }
+        if($_GET['query'] == "login"){ // 登陆请求
+            // 验证登陆态
+            $retval = mysqli_query($connToMysql, "SELECT wxappid, wxsecret FROM wxapp_info WHERE wxappname = '取个号' ");
+            $row = mysqli_fetch_array($retval, MYSQLI_NUM);
+            $wxappid = $row[0];
+            $wxsecret = $row[1];
+            $wxcode = $_GET['code'];
+            $wxgrantType = "authorization_code";
+            $connToWxApi = curl_init();
+            $urlWithGet = "https://api.weixin.qq.com/sns/jscode2session?appid=" . $wxappid . "&secret=" . $wxsecret . "&js_code=" . $wxcode . "&grant_type=" . $wxgrantType;
+            curl_setopt($connToWxApi, CURLOPT_URL, $urlWithGet);
+            curl_setopt($connToWxApi, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($connToWxApi, CURLOPT_HEADER, true);
+            $loginInfo = curl_exec($connToWxApi);
+            $loginInfo = json_decode($loginInfo);
+            // 生成3rd_session
+            $urandFh = fopen("/dev/urandom", "r");
+            $sessionKey = fread($urandFh, 16);
+            fclose($urandFh);
+            // 存储session
+            $retval = mysqli_query($connToMysql, "INSERT INTO session_record (openid, sessionkey, time_session) VALUES (" . $loginInfo['openid'] ", " . $sessionKey ", NOW())");
+            $resultArray = array('loginsuccess' => true, 'sessionkey' => $sessionKey);
+            echo json_encode($resultArray);
+        }else if($_GET['query'] == "seller_list"){ // 商家列表请求
             $retval = mysqli_query($connToMysql, "SELECT COUNT(*) FROM seller_list");
             if(!$retval){
                 echo "Error: " . mysqli_error();
@@ -24,7 +61,9 @@
             }
             $resultArray['list'] = $sellerArray;
             echo json_encode($resultArray);
-        }else if($GET['query'] == "good_list"){
+        }else if($GET['query'] == "good_list"){ // 货单请求
+        }else{
+            echo "Error: 非法请求";
         }
     }
 ?>
--- index.php
+++ index.php
@@ -9,9 +9,7 @@ Description	: A two-column, fixed-width design with dark color scheme.
 Version		: 1.0
 Released	: 20130902
 
-Develop by soar
-https://brocadesoar.cn
-本博客网站样式基于上述开发者(Design by TEMPLATED), 许可证信息见    
+Develop by ZhuBrocadeSoar  
 -->
 
 <!-- SESSION-START -->
@@ -19,9 +17,7 @@ https://brocadesoar.cn
     session_start();
 ?>
 
-<!-- HTML-BEGIN -->
 <html xmlns="http://www.w3.org/1999/xhtml">
-	<!-- HEAD-BEGIN -->	
     <head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />	
 		<meta name="keywords" content="index" />	
@@ -32,9 +28,7 @@ https://brocadesoar.cn
 		<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />	
 		<link href="images/favicon.ico" rel="shortcut icon" />	
 		<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" />< -->	
-	<!-- HEAD-END -->	
 	</head>	
-	<!-- BODY-BEGIN -->	
 	<body>	
 		<div id="page" class="container">	
 			<div id="header">	
@@ -49,8 +43,10 @@ https://brocadesoar.cn
                         <li class="current_page_item"><a href="https://brocadesoar.cn" accesskey="1" title ="">主页</a></li>
                         <li><a href="blog.php" accesskey="2" title="">博客</a></li>
                         <li><a href="bbs.php" accesskey="3" title="">留言</a></li>
+                        <!-->
                         <li><a href="#" accesskey="4" title="">梯子</a></li>
                         <li><a href="#" accesskey="5" title="">传送</a></li>
+                        <-->
                         <li><a href="#" accesskey="6" title="">关于</a></li>
                         <li><a href="login.php" accesskey="7" title="">登陆</a></li>
                     </ul>
@@ -82,7 +78,5 @@ https://brocadesoar.cn
                 </div>
             </div>
 		</div>	
-	<!-- BODY-END -->	
 	</body>	
-<!-- HTML-END -->
 </html>
